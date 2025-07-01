799d6c0570330a6a42572e203dfb0fa567dabf6a
Dharm-dagar <singhdharm31503@gmail.com>
2025-07-01 12:43:41 +0530

    Updated dashboard components and design

--- car-damage-assesment/src/App.jsx
+++ car-damage-assesment/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import LandingPage from './components/LandingPage';
 import Dashboard from './components/Dashboard';
-import DamageAssessment from './components/DamageAssessment';  
+import DamageAssessment from './components/DamageAssessment';
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -13,13 +13,19 @@ function App() {
     return () => clearTimeout(timer);
   }, []);
 
-  
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center text-white bg-black text-2xl">
+        Loading...
+      </div>
+    );
+  }
 
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/damage-assessment" element={<DamageAssessment />} /> {/* Added route */}
+      <Route path="/damage-assessment" element={<DamageAssessment />} />
     </Routes>
   );
 }
